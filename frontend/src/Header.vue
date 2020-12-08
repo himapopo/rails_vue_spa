9f5a94bd23b95db8d90fee6a28680b8404ae86ab
yokokawa taiki <himapopo0507@yahoo.co.jp>
2020-12-08 19:16:55 +0900

    Useモデル作り直し。bcrypt追加

---
+++ frontend/src/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand ml-md-5" href="/">鳥さんの楽園</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-end mr-5" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
