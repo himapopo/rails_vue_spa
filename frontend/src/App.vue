9a2e8d675fb124ef960291f2955fb14428d317ec
yokokawa taiki <himapopo0507@yahoo.co.jp>
2020-12-16 02:47:31 +0900

    nav作成中

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <Header></Header>
+    <span @click="navshow" v-if="!birdnav">検索表示▼</span>
+    <transition name="nav" mode="out-in">
+      <Nav v-if="birdnav"></Nav>
+    </transition>
+    <span @click="navshow" v-if="birdnav">検索非表示▲</span>
     <transition name="flash-fade">
       <div class="col-12 text-center flash" v-if="$store.state.flash.success">
         <span class="text-success">{{ $store.state.flash.success_message }}</span>
@@ -20,17 +25,48 @@
 <script>
 import { mapMutations } from 'vuex'
 import Header from './Header.vue'
+import Nav from './nav'
 export default {
+  data(){
+    return{
+      birdnav: false
+    }
+  },
   components:{
     Header,
+    Nav
   },
   methods:{
     ...mapMutations(['remove_success_message', 'remove_error_message']),
-  },
+    navshow(){
+      this.birdnav = !this.birdnav
+    },
+  }
 }
 </script>
 
 <style scoped>
+.nav-enter{
+  height: 0;
+  opacity: 0;
+}
+.nav-enter-active{
+  transition: all .5s;
+}
+.nav-enter-to{
+  height: 100%;
+}
+.nav-leave{
+  height: 100%;
+}
+.nav-leave-to{
+  height: 0;
+  opacity: 0;
+}
+.nav-leave-active{
+  transition: all .5s;
+}
+
 .flash-fade-enter{
   opacity:0;
 }
