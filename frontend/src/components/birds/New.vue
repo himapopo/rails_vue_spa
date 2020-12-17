f65c99a39fa190715c3443311f1b354c34baeb01
yokokawa taiki <himapopo0507@yahoo.co.jp>
2020-12-17 13:55:41 +0900

    フォームデザイン修正

--- frontend/src/components/birds/New.vue
+++ frontend/src/components/birds/New.vue
@@ -1,58 +1,74 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-10 mx-auto">
+      <div class="col-md-10 mx-auto navber-color">
         <div class="row text-center">
-          <div class="col-12">
+          <div class="col-12 my-3">
             <h1>鳥さん投稿</h1>
           </div>
-          <div class="col-12">
-            <label for="">鳥さんの種類</label><br>
-            <select name="type" id="" v-model="bird.type">
+          <div class="col-12 my-3">
+            <label for="" class="text-success border-bottom pb-1 border-success">鳥さんの種類</label><br>
+            <select class="form-control" name="type" id="" v-model="bird.type">
               <option value="start" disabled>種類を選択してください</option>
               <option v-for="bird in $store.state.birds.birds" :key="bird" :value="bird">
                 {{ bird }}
               </option>
             </select>
-            <span>{{ bird.type }}</span>
           </div>
-          <div class="col-12">
-            <span>鳥さんの大きさ</span><br>
-            <input type="radio" value="大型" v-model="bird.size">
-            <label for="">大型</label>
-            <input type="radio" value="中型" v-model="bird.size">
-            <label for="">中型</label>
-            <input type="radio" value="小型" v-model="bird.size">
-            <label for="">小型</label>
-            {{ bird.size }}
+          <div class="col-12 my-3">
+            <span class="text-success border-bottom pb-1 border-success">鳥さんの大きさ</span><br>
+            <div class="row mt-2">
+              <div class="col-4">
+                <input type="radio" value="大型" id="big" v-model="bird.size">
+                <label for="big">大型</label>
+              </div>
+              <div class="col-4">
+                <input type="radio" value="中型" id="normal" v-model="bird.size">
+                <label for="normal">中型</label>
+              </div>
+              <div class="col-4">
+                <input type="radio" value="小型" id="small" v-model="bird.size">
+                <label for="small">小型</label>
+              </div>
+            </div>
           </div>
-          <div class="col-12">
-            <span>鳥さんの年齢</span><br>
-            <input type="radio" value="10歳未満" v-model="bird.age">
-            <label for="">0〜10歳</label>
-            <input type="radio" value="20歳未満" v-model="bird.age">
-            <label for="">11〜20歳</label>
-            <input type="radio" value="30歳未満" v-model="bird.age">
-            <label for="">21〜30歳</label>
-            <input type="radio" value="31歳以上" v-model="bird.age">
-            <label for="">31歳〜</label>
-            {{ bird.age }}
+          <div class="col-12 my-3">
+            <span class="text-success border-bottom pb-1 border-success">鳥さんの年齢</span><br>
+            <div class="row mt-2">
+              <div class="col-6 col-md-3">
+                <input name="bird-age" type="radio" value="10歳未満" v-model="bird.age">
+                <label for="">0〜10歳</label>
+              </div>
+              <div class="col-6 col-md-3">
+                <input name="bird-age" type="radio" value="20歳未満" v-model="bird.age">
+                <label for="">11〜20歳</label>
+              </div>
+              <div class="col-6 col-md-3">
+                <input name="bird-age" type="radio" value="30歳未満" v-model="bird.age">
+                <label for="">21〜30歳</label>
+              </div>
+              <div class="col-6 col-md-3">
+                <input name="bird-age" type="radio" value="31歳以上" v-model="bird.age">
+                <label for="">31歳〜</label>
+              </div>
+            </div>
           </div>
-          <div class="col-12">
-            <label for="">鳥さん詳細</label><br>
-            <textarea name="" id="" cols="30" rows="10" v-model="bird.details"></textarea>
+          <div class="col-12 my-3">
+            <label for="" class="text-success border-bottom pb-1 border-success">鳥さん詳細</label><br>
+            <textarea name="" id="" cols="30" rows="10" v-model="bird.details" class="form-control"></textarea>
           </div>
-          <div class="col-12">
-            <label for="">鳥さんの画像 ※一枚は必須です</label><br>
+          <div class="col-12 my-3">
+            <label for="" class="text-success border-bottom pb-1 border-success">鳥さんの画像</label><br>
+            <span class="text-danger">※↓一枚目は必須です</span><br>
             <input type="file" @change="imageChange1">
           </div>
-          <div class="col-12">
+          <div class="col-12 my-3">
             <input type="file" @change="imageChange2">
           </div>
-          <div class="col-12">
+          <div class="col-12 my-3">
             <input type="file" @change="imageChange3">
           </div>
-          <div class="col-12">
+          <div class="col-12 my-3">
             <button class="btn btn-primary">投稿</button>
           </div>
         </div>
@@ -75,6 +91,23 @@ export default {
         image3: ""
       }
     }
+  },
+  methods:{
+    imageChange1(){
+
+    },
+    imageChange2(){
+
+    },
+    imageChange3(){
+
+    }
   }
 }
 </script>
+
+<style scoped>
+  .navber-color{
+    background-color: #FFFFDD;
+  }
+</style>
